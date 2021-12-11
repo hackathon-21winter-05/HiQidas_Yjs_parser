b74e293a47a8e013789b86bade71b99f38463faf
logica <84759675+logica0419@users.noreply.github.com>
2021-12-11 17:01:09 +0900

    websocketを実装

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import { Hello } from "./Hello";
+import ReconnectingWebSocket from "reconnecting-websocket";
 
-Hello();
+const env = process.env;
+
+const RWS = new ReconnectingWebSocket(
+  "wss://" + env.SERVER_HOST + "/api/ws/yjs-admin"
+);
+
+RWS.binaryType = "arraybuffer";
+
+RWS.onopen = () => {
+  console.log("connected");
+};
+
+RWS.onclose = () => {
+  console.log("disconnected");
+};
+
+RWS.onerror = (error) => {
+  console.log("error ocurred", error);
+};
+
+export default RWS;
